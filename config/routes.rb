9386c2e894eb0f9f5c3e9e623d2eeb73d395b13d
Jillian Kando <j.kando@gmail.com>
2014-04-03 09:14:26 -0400

    Add user microposts

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ SampleApp::Application.routes.draw do
   match '/contact', to: 'static_pages#contact'
 
   resources :sessions, only: [:new, :create, :destroy]
+  resources :microposts, only: [:create, :destroy]
 
   match '/signup', to: 'users#new'
   match '/signin', to: 'sessions#new'
